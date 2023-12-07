870e5aad4e646e22c47878ac6791a871f6de9c68
Taha Khan <93130261+tahakhan2004@users.noreply.github.com>
2023-12-08 03:04:17 +0500

    Update Instance.js

--- Client/src/Instance.js
+++ Client/src/Instance.js
@@ -1,5 +1,1 @@
-const dev = "http://localhost:5000/api/"
-const prod = "https://odd-teal-snapper-tux.cyclic.app/api/"
- 
-export const BASE_URI = window.location.hostname.split(":")[0] === "localhost" ? dev : prod; 
-export const CLIENT_ID = "565903132155-oqb17r12or14kr3bgadsvakehqsobvtd.apps.googleusercontent.com";
+
