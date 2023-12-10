dadbd18c9af838f143f2ebaebd8c073ff43c5e7a
Taha Khan <93130261+tahakhan2004@users.noreply.github.com>
2023-12-10 16:10:39 +0500

    Update userSchema.js

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -17,6 +17,10 @@ const Schema = mongoose.Schema({
         type: String,
         required: true,
     },
+    contacts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
     email:{
         type: String,
     },
